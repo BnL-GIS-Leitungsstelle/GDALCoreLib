95126fc8af7c3e91c8527b961fce13e1d2051f4a
Mohr Mirco <61457049+mircmo@users.noreply.github.com>
2025-03-19 16:33:11 +0100

    Add functionality to write metadata for specific layers

--- ESRIFileGeodatabaseAPI/FGDBMetadataWriter.cs
+++ ESRIFileGeodatabaseAPI/FGDBMetadataWriter.cs
@@ -12,5 +12,38 @@ namespace ESRIFileGeodatabaseAPI
             table.Close();
             db.Close();
         }
+
+        /// <summary>
+        /// This method copies the metadata from the source data to all layers in the destination data. 
+        /// It works for layers that have the same name in both files.
+        /// </summary>
+        public static void CopyMetadataForAllLayers(string sourceFgdbPath, string destinationFgdbpath)
+        {
+            using var source = Geodatabase.Open(sourceFgdbPath);
+
+            // create a dict with the layername as key and the metadata as the value
+            var layernameDocumentationMap = source.GetChildDatasets("\\", "").ToDictionary(name => name.Trim('\\'), name =>
+            {
+                using var table = source.OpenTable(name);
+                return table.Documentation;
+            });
+
+            WriteMetadata(destinationFgdbpath, layernameDocumentationMap);
+        }
+
+        public static void WriteMetadata(string fgdbPath, IDictionary<string, string> layerMetadataMappings)
+        {
+            using var gdb = Geodatabase.Open(fgdbPath);
+
+            foreach (var layerName in gdb.GetChildDatasets("\\", ""))
+            {
+                var escapedName = layerName.Trim('\\');
+                if (layerMetadataMappings.TryGetValue(escapedName, out string? value))
+                {
+                    using var table = gdb.OpenTable(escapedName);
+                    table.Documentation = value;
+                }
+            }
+        }
     }
 }
