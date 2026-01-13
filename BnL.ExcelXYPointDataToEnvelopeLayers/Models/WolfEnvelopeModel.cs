cb23459905304e214913d619f32270602ffb37fe
Geodata20 <64781947+Geodata20@users.noreply.github.com>
2026-01-13 19:12:44 +0100

    adopt result

--- BnL.ExcelXYPointDataToEnvelopeLayers/Models/WolfEnvelopeModel.cs
+++ BnL.ExcelXYPointDataToEnvelopeLayers/Models/WolfEnvelopeModel.cs
@@ -5,7 +5,9 @@ public class WolfEnvelopeModel
     public string? ObservationDate { get; set; }
     public string? IndividualId { get; set; }
     public int IndividuumCount { get; set; } = 1;
-    public string? CompartmentMain { get; set; }
+
+    public string? RegulationEvents { get; set; } 
+    public string? Compartment { get; set; }
     public string? Canton { get; set; }
     public double? X { get; set; }
     public double? Y { get; set; }
@@ -13,4 +15,9 @@ public class WolfEnvelopeModel
     public double? EnvelopeLowerLeftY { get; set; }
     public double? EnvelopeUpperRightX { get; set; }
     public double? EnvelopeUpperRightY { get; set; }
+
+    public override string ToString()
+    {
+        return $"Canton= {Canton}";
+    }
 }
